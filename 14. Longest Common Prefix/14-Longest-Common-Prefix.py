d3d6fb91e9de6eb0da708c44f141a6f507ce496f
Yomna Ali <32110361+YomnaAhmed97@users.noreply.github.com>
2024-08-08 12:45:43 +0300

    [14] [Time Beats: 88.06%] [Memory Beats: 65.79%] - LeetPush

---
+++ 14. Longest Common Prefix/14-Longest-Common-Prefix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return \\
+        longest_prefix = strs[0]
+        for i in range (1,len(strs)):
+            while strs[i].find(longest_prefix) != 0:
+                longest_prefix = longest_prefix[:-1]
+                if len(longest_prefix) == 0:
+                    return \\
+        return longest_prefix
